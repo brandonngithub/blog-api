23ac5812ab15342ab205e24affe501e5c97df9cb
Brandon Ngo <brandonngo56@gmail.com>
2025-04-27 15:07:31 -0400

    Stub out endpoints

--- app.js
+++ app.js
@@ -2,8 +2,43 @@ const express = require("express");
 
 const app = express();
 
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+
+// create endpoint for creating a new user
+app.post("/user", (req, res) => {});
+
+// read endpoint for getting an existing user
+app.get("/user", (req, res) => {});
+
+
+// create endpoint for a single post
+app.post("/post", (req, res) => {});
+
+// read endpoint for getting all posts
+app.get("/posts", (req, res) => {});
+
+// read endpoint for getting a single post
+app.get("/post/:id", (req, res) => {});
+
+// update endpoint for editing a post
+app.patch("/post/:id", (req, res) => {});
+
+// delete endpoint for deleting a post
+app.delete("/post/:id", (req, res) => {});
+
+
+// create endpoint for creating a new comment
+app.post("/post/:id/comment", (req, res) => {});
+
+// read endpoint for getting all comments for a post
+app.get("/post/:id/comments", (req, res) => {});
+
+// delete endpoint for deleting a comment
+app.delete("/post/:id/comment/:commentId", (req, res) => {});
+
 
 app.listen(3000, () => console.log("Server running on port 3000!"));
